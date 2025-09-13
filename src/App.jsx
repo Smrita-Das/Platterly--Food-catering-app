729dec6f3bb04e3062490e6027a9bae8271b385d
Smrita Das <1003sdas@gmail.com>
2025-09-13 22:27:50 +0530

    Created the navbar and installed shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import {Routes,Route,Link} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Products from './pages/Products'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
     <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/products' element={<Products/>}/>
+    </Routes>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import platerllyLOGO from '../assets/platterly_Logo.png'
 
 function Navbar() {
   return (
-    <div>
-      
+    <>
+    <div className="fixed top-0 w-full bg-white ">
+      <div className="flex px-5 py-1 justify-between items-center">
+
+        <div className="flex items-center gap-2">
+          <img src={platerllyLOGO} alt="" className="h-10 w-10 rounded-full " />
+          <p className="">Platterly by SD</p>
+        </div>
+      <Link to='/about'>About Us</Link>
+      <Link to='/products'>Products/Services</Link>
+      <Link>SignUp</Link>
+
+      </div>
     </div>
+    </>
   )
 }
 
